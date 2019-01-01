319fe58fd7780f47305c2389ee18787f377dfa99
Boris Ettinger <ettinger.boris@gmail.com>
2019-01-02 01:32:11 +0200

    Add tests to noise trader

--- src/test/scala/leveragesim/NoiseTraderSpec.scala
+++ src/test/scala/leveragesim/NoiseTraderSpec.scala
@@ -3,7 +3,7 @@ package leveragesim
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{TestKit, TestProbe}
 import leveragesim.DemandType.NoiseDemand
-import leveragesim.Messages.{Demand, Price}
+import leveragesim.Messages.{Demand, Price, RequestForQuote}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
 
@@ -30,4 +30,39 @@ class NoiseTraderSpec(_system: ActorSystem)
        case Demand(x: Double, NoiseDemand, `testNoiseTrader`) => Math.abs(x - 4.76635) should be <= 5e-6
     }
   }
+
+  "A noise trader actor" should "reply to second price message with different demand" in {
+    val testSim = new Simulation(fundamentalValue=1, totalValue=1)
+    val testProbe = TestProbe()
+    val testProbe2 = TestProbe()
+    val message = Price(1, testProbe.ref)
+    val message2 = Price(1, testProbe2.ref)
+    val testNoiseTrader = system.actorOf(Props(new NoiseTrader(rho=1, sigma=1, sim=testSim, seed=1)))
+    testNoiseTrader ! message
+    testNoiseTrader ! message2
+    testProbe2.expectMsgPF() {
+      case Demand(x: Double, NoiseDemand, `testNoiseTrader`) => Math.abs(x - 2.59451) should be <= 5e-6
+    }
+  }
+
+  "A noise trader actor" should "reply to request for quote message with the same demand" in {
+    val testSim = new Simulation(fundamentalValue=1, totalValue=1)
+    val testProbe = TestProbe()
+    val testProbe2 = TestProbe()
+    val testProbe3 = TestProbe()
+    val message = Price(1, testProbe.ref)
+    val message2 = RequestForQuote(1, testProbe2.ref)
+    val message3 = RequestForQuote(1, testProbe3.ref)
+    val testNoiseTrader = system.actorOf(Props(new NoiseTrader(rho=1, sigma=1, sim=testSim, seed=1)))
+    testNoiseTrader ! message
+    testNoiseTrader ! message2
+    testNoiseTrader ! message3
+    testProbe2.expectMsgPF() {
+      case Demand(x: Double, NoiseDemand, `testNoiseTrader`) => Math.abs(x - 4.76635) should be <= 5e-6
+    }
+    testProbe3.expectMsgPF() {
+      case Demand(x: Double, NoiseDemand, `testNoiseTrader`) => Math.abs(x - 4.76635) should be <= 5e-6
+    }
+  }
+
 }
